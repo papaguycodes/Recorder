ca4b04c36e36cf1379c4a94ebe1ec4a6e39e5e2c
PaPaGuy Codes  / <175421249+PaPaGuyCodes@users.noreply.github.com>
2024-08-11 22:52:07 +0100

    Create SplashActivity.kt

---
+++ app/src/main/java/com/papaguycodes/recorder/SplashActivity.kt
@@ -0,0 +1,23 @@
+package com.papaguycodes.recorder
+
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import androidx.appcompat.app.AppCompatActivity
+
+class SplashActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+
+        // Delay and move to MainActivity
+        Handler(Looper.getMainLooper()).postDelayed({
+            val intent = Intent(this@SplashActivity, MainActivity::class.java)
+            startActivity(intent)
+            finish()
+            overridePendingTransition(R.anim.fade_in, R.anim.fade_out)
+        }, 2000) // 2-second delay
+    }
+}
